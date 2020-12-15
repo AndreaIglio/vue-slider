a7568614a37d8e16328fb3c9bbcb6a7edbd92b05
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-15 16:47:18 +0100

    Added dots

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let app = new Vue({
   el: "#app",
   data: {
     counter: 0,
-    timer: null,
     images: [
       "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
       "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
@@ -16,20 +15,20 @@ let app = new Vue({
   },
 
   methods: {
-    next: function() {
+    next: function () {
       this.counter++;
       if (this.counter >= this.images.length) {
         this.counter = 0;
       }
     },
-    prev: function() {
+    prev: function () {
       this.counter--;
       if (this.counter < 0) {
         this.counter = this.images.length - 1;
       }
     },
-    timingSlide(){
-      this.timer == setInterval(this.next,2000);
-    }
+    timingSlide() {
+      setInterval(this.next, 2000);
+    },
   },
 });
