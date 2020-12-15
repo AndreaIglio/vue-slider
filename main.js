c60fc817489ccab5244e6f1ee45de76202be37b8
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-15 15:28:00 +0100

    Added main files + first task for slider
    still need to implement the automatic timing function to the slider

---
+++ main.js
@@ -0,0 +1,30 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    counter: 0,
+    timer: null,
+    images: [
+      "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+      "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
+    ],
+  },
+  methods: {
+    next: function() {
+      this.counter++;
+      if (this.counter >= this.images.length) {
+        this.counter = 0;
+      }
+    },
+    prev: function() {
+      this.counter--;
+      if (this.counter < 0) {
+        this.counter = this.images.length - 1;
+      }
+    },
+    timing: function(){
+      this.timer == setInterval(this.next,2000);
+    }
+  },
+});
